e8a9151349c1015b3ae74389557dc868a6fb157b
IVAM GALVAO FILHO <programador.ivam@gmail.com>
2023-08-09 19:45:24 -0300

    Update execute.sh
    Settings execute

--- execute.sh
+++ execute.sh
@@ -8,8 +8,8 @@ python_script_2="/BILSTM-CE-Model-01.py"
 python_script_3="/BILSTM-CE-Model-CV-01.py"
 
 # Executa os arquivos Python
-python3 "$python_script_1"
+python3 "$python_script_1" &&
 
-python3 "$python_script_2"
+python3 "$python_script_2" &&
 
 python3 "$python_script_3"
